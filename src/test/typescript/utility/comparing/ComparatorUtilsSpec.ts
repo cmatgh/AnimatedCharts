87b5eae2672360a7c0d57ff7bb2464d89c773531
chris <chrizz.m@gmx.de>
2020-05-29 12:58:48 +0200

    add tests for ComparatorUtils

--- src/test/typescript/utility/comparing/ComparatorUtilsSpec.ts
+++ src/test/typescript/utility/comparing/ComparatorUtilsSpec.ts
@@ -1,13 +1,57 @@
 import { expect } from "chai";
+import {Comparator} from "../../../../main/typescript/animatedcharts/utility/comparing/Comparator";
+import {ComparatorUtils} from "../../../../main/typescript/animatedcharts/utility/comparing/ComparatorUtils";
 
 describe("ComparatorUtils", () => {
 
-    beforeEach( () => {
-
-    });
-
     describe("reverse", () => {
-        //TODO
+        it("should give a result of zero if the original comparator returns zero", () => {
+            // given
+            const comparator = new (class TestComparator implements Comparator<any> {
+                compare(o1: any, o2: number): any {
+                    return 0;
+                }
+            })
+
+            // when
+           const reversedComparator = ComparatorUtils.reverse(comparator);
+
+            // then
+            expect(reversedComparator.compare({},{})).to.be.eq(0);
+
+        });
+
+        it("should return the inverse when the original comparator returns a positive number", () => {
+            // given
+            const comparator = new (class TestComparator implements Comparator<any> {
+                compare(o1: any, o2: number): any {
+                    return 5;
+                }
+            })
+
+            // when
+           const reversedComparator = ComparatorUtils.reverse(comparator);
+
+            // then
+            expect(reversedComparator.compare({},{})).to.be.eq(-5);
+
+        });
+
+        it("should return the inverse when the original comparator returns a negative number", () => {
+            // given
+            const comparator = new (class TestComparator implements Comparator<any> {
+                compare(o1: any, o2: number): any {
+                    return -5;
+                }
+            })
+
+            // when
+           const reversedComparator = ComparatorUtils.reverse(comparator);
+
+            // then
+            expect(reversedComparator.compare({},{})).to.be.eq(5);
+
+        })
     });
 
 });
