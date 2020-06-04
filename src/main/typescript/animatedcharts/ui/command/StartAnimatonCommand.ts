8699f697fe5f97326c232f6b27fbab8b3d4a9713
chris <chrizz.m@gmx.de>
2020-06-04 02:14:13 +0200

    Add further commands

---
+++ src/main/typescript/animatedcharts/ui/command/StartAnimatonCommand.ts
@@ -0,0 +1,16 @@
+import {Command} from "./Command";
+import {Animation} from "../../animation/Animation";
+
+export class StartAnimatonCommand implements Command {
+
+    private animation : Animation;
+
+    constructor(animation : Animation) {
+        this.animation = animation;
+    }
+
+    execute(event: Event): void {
+        this.animation.start();
+    }
+
+}
