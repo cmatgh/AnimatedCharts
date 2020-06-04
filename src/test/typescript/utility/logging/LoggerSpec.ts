af82da17a522c4af7f177cf7228ad61f994d01b6
chris <chrizz.m@gmx.de>
2020-06-04 05:02:05 +0200

    Implement logger functionality and add tests

--- src/test/typescript/utility/logging/LoggerSpec.ts
+++ src/test/typescript/utility/logging/LoggerSpec.ts
@@ -1,29 +1,92 @@
-import { expect } from "chai";
+import {Logger} from "../../../../main/typescript/animatedcharts/utility/logging/Logger";
+import {LogLevel} from "../../../../main/typescript/animatedcharts/utility/logging/LogLevel";
+import {expect} from "chai";
 
 describe("Logger", () => {
 
+    let logger : Logger = Logger.getInstance();
+    let debugMock: any;
+    let infoMock: any;
+    let warnMock: any;
+    let errorMock: any;
+
     beforeEach( () => {
-
-    });
-
-    describe("compare", () => {
-        //TODO
+        debugMock = mockConsole("debug");
+        warnMock = mockConsole("warn");
+        infoMock = mockConsole("info");
+        errorMock = mockConsole("error");
     });
 
     describe("debug", () => {
-        //TODO
+
+        it("should log correctly", () => {
+            logger.setLogLevel(LogLevel.DEBUG);
+            logger.debug("message");
+            expect(debugMock.called).to.be.true;
+            expect(debugMock.value).to.contain("DEBUG");
+        });
+
     });
 
     describe("info", () => {
-        //TODO
+        it("should log correctly", () => {
+            logger.info("message");
+            expect(infoMock.called).to.be.true;
+            expect(infoMock.value).to.contain("INFO");
+        });
     });
 
     describe("warn", () => {
-        //TODO
+        it("should log correctly", () => {
+            logger.warn("message");
+            expect(warnMock.value).to.contain("WARN");
+            expect(warnMock.called).to.be.true;
+        });
     });
 
-    describe("severe", () => {
-        //TODO
+    describe("error", () => {
+        it("should log correctly", () => {
+            logger.error("message");
+            expect(errorMock.called).to.be.true;
+            expect(errorMock.value).to.contain("ERROR");
+        });
     });
 
+    describe("LogLevel", () => {
+        it("should log correctly", () => {
+            logger.setLogLevel(LogLevel.ERROR);
+
+            logger.debug("message");
+            logger.info("message");
+            logger.warn("message");
+            logger.error("message");
+            expect(debugMock.called).to.be.false;
+            expect(infoMock.called).to.be.false;
+            expect(warnMock.called).to.be.false;
+            expect(errorMock.called).to.be.true;
+            expect(errorMock.value).to.contain("ERROR");
+        });
+    });
+
+    afterEach(() => {
+        debugMock.restore();
+        infoMock.restore();
+        warnMock.restore();
+        errorMock.restore();
+    })
+
+    function mockConsole(level : string) {
+        let origConsole = console[level];
+        let result = {
+            called: false,
+            value: null,
+            restore : () => { console[level] = origConsole; }
+        };
+        console[level] = (msg: string, arg: string) => {
+            result.value = msg;
+            result.called = true;
+        }
+
+        return result;
+    }
 });
